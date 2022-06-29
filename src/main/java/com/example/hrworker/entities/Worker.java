6e7c2065f1ace7b457ea727c42ed85ff35adff7a
Bruno Escritório <brunosmendes89@gmail.com>
2022-06-29 15:21:09 -0300

    hr-worker created and implemented

---
+++ src/main/java/com/example/hrworker/entities/Worker.java
@@ -0,0 +1,61 @@
+package com.example.hrworker.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+    private static final long serialVersionUID = 1L; //para que o objeto possa ser transformado em bytes para passar na rede
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private Double dailyIncome;
+
+    public Worker(){}
+
+    public Worker(long id, String name, Double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Worker worker = (Worker) o;
+        return id == worker.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
